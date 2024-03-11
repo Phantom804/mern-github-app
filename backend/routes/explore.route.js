61dff86c1b5858720502cb26d270f0116ea32086
Phantom804 <asimarain184@gmail.com>
2024-03-11 23:38:22 +0500

    Update file.js with new changes

--- backend/routes/explore.route.js
+++ backend/routes/explore.route.js
@@ -4,6 +4,6 @@ import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
 
 const router = express.Router();
 
-router.get("/repos/:language", ensureAuthenticated, explorePopularRepos);
+router.get("/repos/:language", explorePopularRepos);
 
 export default router;
